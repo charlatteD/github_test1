cee8323e1602d8f29511fbb964055c8bec2d2e4c
Huiyu Deng <huiyudeng@Huiyus-iMac.lan>
2022-06-13 15:19:04 -0700

    update

--- library_test1.py
+++ library_test1.py
@@ -2,4 +2,10 @@ import pandas as pd
 import numpy as np
 
 test1 = pd.read_csv('/Users/hdeng/Desktop/Random/data_ml_framework/ml_framework/demos/auto-mpg.csv')
+<<<<<<< HEAD
 test1.head(10)
+=======
+test1.head(10)
+
+print('Updated')
+>>>>>>> 500b852... update
